65e7f336cabe176a61341fe3c4e2cf14cd9acaec
eran Sevil <eransev@clalit.org.il>
2024-01-29 16:53:00 +0200

    formet prettier autosave

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
